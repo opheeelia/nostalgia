62ba453f7db14ee5680b701357477b1106db2d66
Ophelia <wattinytoffee@gmail.com>
2021-01-29 20:59:18 -0800

    fix env typo

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 sqldb.init_app(app)
 login_manager = LoginManager(app)
 dbInterface = Database(current_user)
