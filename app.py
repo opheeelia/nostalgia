a7ee1ca15ffd519f65f4d6e27c12b0bed80d5444
Ophelia <wattinytoffee@gmail.com>
2021-01-29 23:56:47 -0800

    fix redirect code

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-conn = psycopg2.connect(os.environ.get('DATABASE_URL'), sslmode='require')
+# conn = psycopg2.connect(os.environ.get('DATABASE_URL'), sslmode='require')
 sqldb.init_app(app)
 login_manager = LoginManager(app)
 dbInterface = Database(current_user)
@@ -143,7 +143,7 @@ def add_song():
 
     dbInterface.add_song(name=name, artist=artist, desc=desc, period=period, year=year, date=date, song_id=spotify_id,
                          saved=True, image=image, preview=preview, link=link)
-    return redirect('/browse'), 200
+    return redirect('/browse', code=302)
 
 
 @app.route('/resetSaved')
